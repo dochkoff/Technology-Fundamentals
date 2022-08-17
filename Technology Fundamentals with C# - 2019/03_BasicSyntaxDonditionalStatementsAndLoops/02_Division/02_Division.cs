324a84bd035700a1b958b9a33c11262bb8cfdc1c
Dochkoff <hounds.07cinder@icloud.com>
2022-08-18 01:19:31 +0300

    03-01,02

---
+++ Technology Fundamentals with C# - 2019/03_BasicSyntaxDonditionalStatementsAndLoops/02_Division/02_Division.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _02_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int divider = 0;
+            int dividerBigger = 0;
+
+            if (number % 2 == 0)
+            {
+                divider = 2;
+                dividerBigger = divider;
+            }
+            if (number % 3 == 0)
+            {
+                divider = 3;
+                dividerBigger = divider;
+            }
+            if (number % 6 == 0)
+            {
+                divider = 6;
+
+                if (divider > dividerBigger)
+                {
+                    dividerBigger = divider;
+                }
+            }
+            if (number % 7 == 0)
+            {
+                divider = 7;
+
+                if (divider > dividerBigger)
+                {
+                    dividerBigger = divider;
+                }
+            }
+            if (number % 10 == 0)
+            {
+                divider = 10;
+
+                if (divider > dividerBigger)
+                {
+                    dividerBigger = divider;
+                }
+            }
+
+            if (dividerBigger == 0)
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by {dividerBigger}");
+            }
+        }
+    }
+}
+
