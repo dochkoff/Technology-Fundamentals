d4d48645d72a474e64a503399517390305d2ec46
Dochkoff <hounds.07cinder@icloud.com>
2022-08-18 01:33:27 +0300

    03-04

---
+++ Technology Fundamentals with C# - 2019/03_BasicSyntaxDonditionalStatementsAndLoops/04_PrintAndSum/4_PrintAndSum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _04_PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
