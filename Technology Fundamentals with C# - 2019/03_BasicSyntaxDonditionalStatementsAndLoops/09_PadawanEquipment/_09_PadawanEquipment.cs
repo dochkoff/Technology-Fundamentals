08bb94bd079c544dbdf25b4377522a29d44abc5a
Dochkoff <hounds.07cinder@icloud.com>
2022-08-18 20:43:28 +0300

    03-09

---
+++ Technology Fundamentals with C# - 2019/03_BasicSyntaxDonditionalStatementsAndLoops/09_PadawanEquipment/_09_PadawanEquipment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09_PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double students = double.Parse(Console.ReadLine());
+            double lightsaberPrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+
+            double lightsaberSum = Math.Ceiling(students * 1.1) * lightsaberPrice;
+            double robeSum = students * robePrice;
+            //double beltSum = (students - Math.Floor(students / 6)) * robePrice;
+            double beltSum1 = Math.Floor(students / 6);
+            double beltSum2 = students - beltSum1;
+            double beltSum3 = beltSum2 * robePrice;
+
+            double equipmentSum = lightsaberSum + robeSum + beltSum1;
+
+            if (budget >= equipmentSum)
+            {
+                Console.WriteLine($"The money is enough - it would cost {equipmentSum:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {equipmentSum - budget:F2}lv more.");
+            }
+        }
+    }
+}
+
