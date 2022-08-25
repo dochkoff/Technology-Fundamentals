3df331502898670f9258956606df06b6559fc7d3
Pavel Dochkov <Dochkoff@DFF-MBP.local>
2022-08-25 23:59:10 +0300

    06

---
+++ Technology Fundamentals with C# - 2019/06_DataTypesAndVariables/P01_ConvertMetersToKilometers/P01_ConvertMetersToKilometers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P01_ConvertMetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+            double kilometers = Math.Round(meters / 1000,2);
+            Console.WriteLine($"{kilometers:F2}");
+        }
+    }
+}
---
+++ Technology Fundamentals with C# - 2019/06_DataTypesAndVariables/P02_PoundsToDollars/P02_PoundsToDollars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P02_PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            decimal dollars = pounds * 1.31M;
+            Console.WriteLine($"{dollars:F3}");
+        }
+    }
+}
