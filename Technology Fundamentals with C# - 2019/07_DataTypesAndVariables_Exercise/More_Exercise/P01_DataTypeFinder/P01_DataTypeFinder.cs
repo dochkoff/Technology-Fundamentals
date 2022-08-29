574d1e8b77b72dc13a9268611f139f0df4338d30
dochkoff <hounds.07cinder@icloud.com>
2022-08-29 23:50:52 +0300

    07-More-01

--- Technology Fundamentals with C# - 2019/07_DataTypesAndVariables_Exercise/More_Exercise/P01_DataTypeFinder/P01_DataTypeFinder.cs
+++ Technology Fundamentals with C# - 2019/07_DataTypesAndVariables_Exercise/More_Exercise/P01_DataTypeFinder/P01_DataTypeFinder.cs
@@ -6,7 +6,37 @@ namespace P01_DataTypeFinder
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+
+            int dataTypeInt;
+            float dataTypeFloat;
+            char dataTypeChar;
+
+            while (input!="END")
+            {
+                if (int.TryParse(input, out dataTypeInt))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (float.TryParse(input, out dataTypeFloat))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input, out dataTypeChar))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (input == "true" || input == "false")
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+
+                input = Console.ReadLine();
+            }
         }
     }
 }
