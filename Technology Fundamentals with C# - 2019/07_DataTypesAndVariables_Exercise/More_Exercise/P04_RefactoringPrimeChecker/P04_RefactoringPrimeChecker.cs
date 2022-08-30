b76e3236e2e09e6e29e6a1c74b4532d6ef0b95d5
dochkoff <hounds.07cinder@icloud.com>
2022-08-30 22:36:43 +0300

    07_More-03,04

---
+++ Technology Fundamentals with C# - 2019/07_DataTypesAndVariables_Exercise/More_Exercise/P04_RefactoringPrimeChecker/P04_RefactoringPrimeChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P04_RefactoringPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = 2; currentNumber <= inputNumber; currentNumber++)
+            {
+                bool isPrime = true;
+
+                for (int delitel = 2; delitel < currentNumber; delitel++)
+                {
+                    if (currentNumber % delitel == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine("{0} -> {1}", currentNumber, isPrime);
+            }
+
+        }
+    }
+}
