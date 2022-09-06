f41e733207aa75bad65d182f0fd2dee14cf5b842
dochkoff <hounds.07cinder@icloud.com>
2022-09-06 19:36:57 +0300

    10-05 correction

--- Technology Fundamentals with C# - 2019/10_Arrays_Exercise/Exercise/P05_TopIntegers/P05_TopIntegers.cs
+++ Technology Fundamentals with C# - 2019/10_Arrays_Exercise/Exercise/P05_TopIntegers/P05_TopIntegers.cs
@@ -7,7 +7,30 @@ namespace P05_TopIntegers
     {
         static void Main(string[] args)
         {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+            string topIntegers = string.Empty;
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                bool isBigger = true;
+                for (int x = i + 1; x < input.Length; x++)
+                {
+                    if (input[i] <= input[x])
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+
+                if (isBigger == true)
+                {
+                    topIntegers += input[i] + " ";
+                }
+            }
+
+            Console.WriteLine(topIntegers);
         }
     }
 }
