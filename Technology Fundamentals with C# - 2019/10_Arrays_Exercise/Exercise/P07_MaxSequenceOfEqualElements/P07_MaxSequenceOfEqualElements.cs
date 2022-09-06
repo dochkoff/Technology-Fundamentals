224e80f0bfe23a6fae0fe61b0477ed012912e440
dochkoff <hounds.07cinder@icloud.com>
2022-09-06 21:46:47 +0300

    10-05, 06, 07

---
+++ Technology Fundamentals with C# - 2019/10_Arrays_Exercise/Exercise/P07_MaxSequenceOfEqualElements/P07_MaxSequenceOfEqualElements.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace P07_MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sequenceNum = 0;
+            int maxSequence = 0;
+            int number = 0;
+            int counter = 1;
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                //for (int x = i + 1; x < input.Length; x++)
+                //{
+                if (input[i] == input[i + 1])
+                {
+                    number = input[i];
+                    counter++;
+
+                    if (counter > maxSequence)
+                    {
+                        maxSequence = counter;
+                        sequenceNum = number;
+                    }
+                }
+                else
+                {
+                    number = 0;
+                    counter = 1;
+                }
+                //}
+            }
+
+            while (maxSequence > 0)
+            {
+                Console.Write(sequenceNum + " ");
+                maxSequence--;
+            }
+        }
+    }
+}
