8fd923c10981442778d4ca2a66ea60ef9edcb70d
dochkoff <hounds.07cinder@icloud.com>
2022-09-14 23:53:23 +0300

    12-P02

--- Technology Fundamentals with C# - 2019/12_MethodsFunctions/P01_SignOfIntegerNumbers/P01_SignOfIntegerNumbers.cs
+++ Technology Fundamentals with C# - 2019/12_MethodsFunctions/P01_SignOfIntegerNumbers/P01_SignOfIntegerNumbers.cs
@@ -7,7 +7,23 @@ namespace P01_SignOfIntegerNumbers
     {
         static void Main(string[] args)
         {
+            SignOfIntegerNumbers(int.Parse(Console.ReadLine()));
 
+            static void SignOfIntegerNumbers(int number)
+            {
+                if (number < 0)
+                {
+                    Console.WriteLine($"The number {number} is negative.");
+                }
+                else if (number == 0)
+                {
+                    Console.WriteLine($"The number {number} is zero.");
+                }
+                else if (number > 0)
+                {
+                    Console.WriteLine($"The number {number} is positive.");
+                }
+            }
         }
     }
 }
---
+++ Technology Fundamentals with C# - 2019/12_MethodsFunctions/P02_Grades/P02_Grades.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P02_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            CheckGrade(grade);
+        }
+    }
+}
