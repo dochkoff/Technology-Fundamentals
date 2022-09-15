a4242f404681ee61c8dc0d995432186569a99bea
dochkoff <hounds.07cinder@icloud.com>
2022-09-15 23:21:50 +0300

    12-P03, P04

---
+++ Technology Fundamentals with C# - 2019/12_MethodsFunctions/P03_Calculations/P03_Calculations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace P03_Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            //add, multiply, subtract, divide
+            if (command == "add")
+            {
+                Add(number1, number2);
+            }
+            else if (command == "multiply")
+            {
+                Multiply(number1, number2);
+            }
+            else if (command == "substract")
+            {
+                Substract(number1, number2);
+            }
+            else if (command == "divide")
+            {
+                Divide(number1, number2);
+            }
+        }
+
+        private static void Add(int number1, int number2)
+        {
+            Console.WriteLine(number1 + number2);
+        }
+
+        private static void Substract(int number1, int number2)
+        {
+            Console.WriteLine(number1 - number2);
+        }
+
+        private static void Multiply(int number1, int number2)
+        {
+            Console.WriteLine(number1 * number2);
+        }
+
+        private static void Divide(int number1, int number2)
+        {
+            Console.WriteLine(number1 / number2);
+        }
+
+    }
+}
---
+++ Technology Fundamentals with C# - 2019/12_MethodsFunctions/P04_PrintingTriangle/P04_PrintingTriangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P04_PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            PrintRectangle(inputNumber);
+            PrintRectangleReverced(inputNumber - 1);
+        }
+
+        private static void PrintRectangle(int inputNumber)
+        {
+            for (int col = 1; col <= inputNumber; col++)
+            {
+                for (int row = 1; row <= col; row++)
+                {
+                    Console.Write(row + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void PrintRectangleReverced(int inputNumber)
+        {
+            for (int col = inputNumber; col >= 1; col--)
+            {
+                for (int row = 1; row <= col; row++)
+                {
+                    Console.Write(row + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
