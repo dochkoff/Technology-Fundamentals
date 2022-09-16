6299d52dd3355699e2b7825e9746f1b60fd7474f
dochkoff <hounds.07cinder@icloud.com>
2022-09-17 01:03:24 +0300

    T06-update

---
+++ Technology Fundamentals with C# - 2022/T06_BasicSyntaxDonditionalStatementsAndLoops_Exercise/Exercise/P04_PrintAndSum/P04_PrintAndSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P04_PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = num1; i <= num2; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.Write($"Sum: {sum}");
+        }
+    }
+}
+
---
+++ Technology Fundamentals with C# - 2022/T06_BasicSyntaxDonditionalStatementsAndLoops_Exercise/Exercise/P06_StrongNumber/P06_StrongNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P06_StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numer = int.Parse(Console.ReadLine());
+
+            int workNumber = numer;
+            int divider = 10;
+            int factorielDigit = 1;
+            int totalSum = 0;
+
+            for (int i = 0; i < numer.ToString().Length; i++)
+            {
+                workNumber %= 10;
+                for (int x = 1; x <= workNumber; x++)
+                {
+                    factorielDigit *= x;
+                }
+                totalSum += factorielDigit;
+
+                workNumber = numer / divider;
+                divider *= 10;
+                factorielDigit = 1;
+
+            }
+
+            if (totalSum == numer)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
+
