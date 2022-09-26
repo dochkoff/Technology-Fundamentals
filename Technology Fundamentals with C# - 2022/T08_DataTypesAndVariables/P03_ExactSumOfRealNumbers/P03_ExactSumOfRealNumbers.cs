09df46a8c25fbaa16246c126739c48c4f72e2a7b
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-09-26 21:25:20 +0300

    T08

---
+++ Technology Fundamentals with C# - 2022/T08_DataTypesAndVariables/P03_ExactSumOfRealNumbers/P03_ExactSumOfRealNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace P03_ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            decimal sum=0M;
+            for (int i = 0; i < counter; i++)
+            {
+                decimal input = decimal.Parse(Console.ReadLine());
+                sum += input;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
