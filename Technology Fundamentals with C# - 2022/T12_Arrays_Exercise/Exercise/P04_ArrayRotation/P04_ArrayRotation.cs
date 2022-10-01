dd44f7d1c539bb3adc6fd623f305be86dad6b438
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-10-01 22:32:44 +0300

    T12-P04

---
+++ Technology Fundamentals with C# - 2022/T12_Arrays_Exercise/Exercise/P04_ArrayRotation/P04_ArrayRotation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotations; i++)
+            {
+                string tempInput = input[0];
+
+                for (int x = 0; x < input.Length - 1; x++)
+                {
+
+                    input[0 + x] = input[1 + x];
+
+                }
+                input[input.Length - 1] = tempInput;
+            }
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
