38a8f55f33337d327776b73e913e82efc0829795
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-10-01 22:31:40 +0300

    T12-P01

---
+++ Technology Fundamentals with C# - 2022/T12_Arrays_Exercise/Exercise/P01_Train/P01_Train.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonCount = int.Parse(Console.ReadLine());
+
+            int[] peopleInWagon = new int[wagonCount];
+            int sum = 0;
+
+            for (int i = 0; i < wagonCount; i++)
+            {
+                peopleInWagon[i] = int.Parse(Console.ReadLine());
+                sum += peopleInWagon[i];
+            }
+
+            string allPeopleInWagons = string.Join(" ", peopleInWagon);
+
+            Console.WriteLine(allPeopleInWagons);
+            Console.WriteLine(sum);
+        }
+    }
+}
