67e3e2e901decbd830c8944dd4129b7caea46964
dochkoff <hounds.07cinder@icloud.com>
2022-10-10 16:47:14 +0300

    T14-P07

---
+++ Technology Fundamentals with C# - 2022/T14_Methods/P07_RepeatString/P07_RepeatString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P07_RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int repeat = int.Parse(Console.ReadLine());
+            PrintConcatenatedText(text, repeat);
+        }
+
+        private static void PrintConcatenatedText(string text, int repeat)
+        {
+            string concatenatedText = null;
+
+            for (int i = 1; i <= repeat; i++)
+            {
+                concatenatedText += text;
+            }
+            Console.WriteLine(concatenatedText);
+        }
+    }
+}
