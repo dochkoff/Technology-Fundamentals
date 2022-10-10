b94c6878663bb36a563b1765f03ed5ec7038d40a
dochkoff <hounds.07cinder@icloud.com>
2022-10-10 16:20:36 +0300

    T14-P05

---
+++ Technology Fundamentals with C# - 2022/T14_Methods/P05_Orders/P05_Orders.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P05_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            Order(product, quantity);
+        }
+
+        private static void Order(string product, int quantity)
+        {
+            double productPrice = 0;
+
+            switch (product)
+            {
+                case "coffee":
+                    productPrice = 1.5;
+                    break;
+                case "water":
+                    productPrice = 1;
+                    break;
+                case "coke":
+                    productPrice = 1.4;
+                    break;
+                case "snacks":
+                    productPrice = 2;
+                    break;
+                default:
+                    Console.WriteLine("There is no such product.");
+                    break;
+            }
+
+            double totalPrice = productPrice * quantity;
+
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+    }
+}
