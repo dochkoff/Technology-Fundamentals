4f9fd25a84d5596493691eff8b64b48dba47f42c
dochkoff <hounds.07cinder@icloud.com>
2022-10-10 13:43:43 +0300

    T14-P04

--- Technology Fundamentals with C# - 2022/T14_Methods/P03_Calculations/P03_Calculations.cs
+++ Technology Fundamentals with C# - 2022/T14_Methods/P03_Calculations/P03_Calculations.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace P03_Calculations
 {
@@ -7,7 +6,48 @@ namespace P03_Calculations
     {
         static void Main(string[] args)
         {
+            string command = Console.ReadLine();
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
 
+            //add, multiply, subtract, divide
+            if (command == "add")
+            {
+                Add(number1, number2);
+            }
+            else if (command == "multiply")
+            {
+                Multiply(number1, number2);
+            }
+            else if (command == "substract")
+            {
+                Substract(number1, number2);
+            }
+            else if (command == "divide")
+            {
+                Divide(number1, number2);
+            }
         }
+
+        private static void Add(int number1, int number2)
+        {
+            Console.WriteLine(number1 + number2);
+        }
+
+        private static void Substract(int number1, int number2)
+        {
+            Console.WriteLine(number1 - number2);
+        }
+
+        private static void Multiply(int number1, int number2)
+        {
+            Console.WriteLine(number1 * number2);
+        }
+
+        private static void Divide(int number1, int number2)
+        {
+            Console.WriteLine(number1 / number2);
+        }
+
     }
 }
