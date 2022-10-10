23205c17be89347c4dc40323df5ef2d61ce3c414
dochkoff <hounds.07cinder@icloud.com>
2022-10-10 13:39:56 +0300

    T14_P02

--- Technology Fundamentals with C# - 2022/T14_Methods/P01_SignOfIntegerNumbers/P01_SignOfIntegerNumbers.cs
+++ Technology Fundamentals with C# - 2022/T14_Methods/P01_SignOfIntegerNumbers/P01_SignOfIntegerNumbers.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace P01_SignOfIntegerNumbers
 {
@@ -7,7 +6,23 @@ namespace P01_SignOfIntegerNumbers
     {
         static void Main(string[] args)
         {
+            SignOfIntegerNumbers(int.Parse(Console.ReadLine()));
 
+            static void SignOfIntegerNumbers(int number)
+            {
+                if (number < 0)
+                {
+                    Console.WriteLine($"The number {number} is negative.");
+                }
+                else if (number == 0)
+                {
+                    Console.WriteLine($"The number {number} is zero.");
+                }
+                else if (number > 0)
+                {
+                    Console.WriteLine($"The number {number} is positive.");
+                }
+            }
         }
     }
 }
---
+++ Technology Fundamentals with C# - 2022/T14_Methods/P02_Grades/P02_Grades.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
