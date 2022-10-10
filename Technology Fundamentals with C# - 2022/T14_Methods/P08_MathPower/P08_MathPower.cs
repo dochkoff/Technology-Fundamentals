49325a1847be7c9579644afbd60b0b3fbae4933f
dochkoff <hounds.07cinder@icloud.com>
2022-10-10 17:01:52 +0300

    T14_P08

---
+++ Technology Fundamentals with C# - 2022/T14_Methods/P08_MathPower/P08_MathPower.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P08_MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = RaiseToPower(number, power);
+            Console.WriteLine(result);
+
+            static double RaiseToPower(double number, int power)
+            {
+                double result = Math.Pow(number, power);
+                return result;
+            }
+        }
+    }
+}
