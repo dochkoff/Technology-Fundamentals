7109194e0cf865bdd24c2919d803e39448fd5593
dochkoff <hounds.07cinder@icloud.com>
2022-10-11 19:45:45 +0300

    T14-P11

---
+++ Technology Fundamentals with C# - 2022/T14_Methods/P11_MathOperations/P11_MathOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace P11_MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            int number2 = int.Parse(Console.ReadLine());
+
+            double result = Calculate(number1, @operator, number2);
+
+            Console.WriteLine(result);
+        }
+
+        private static double Calculate(int number1, string @operator, int number2)
+        {
+            double calculation = 0;
+
+            switch (@operator)
+            {
+                case "/":
+                    calculation = number1 / number2;
+                    break;
+                case "*":
+                    calculation = number1 * number2;
+                    break;
+                case "+":
+                    calculation = number1 + number2;
+                    break;
+                case "-":
+                    calculation = number1 - number2;
+                    break;
+            }
+
+            return calculation;
+        }
+    }
+}
