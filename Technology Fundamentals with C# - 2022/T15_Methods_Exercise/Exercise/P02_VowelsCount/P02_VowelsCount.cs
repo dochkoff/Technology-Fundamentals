62ec014b367cb99b6c5b0d46f6f685a895fd0cca
dochkoff <hounds.07cinder@icloud.com>
2022-10-15 22:54:52 +0300

    T15-P02

---
+++ Technology Fundamentals with C# - 2022/T15_Methods_Exercise/Exercise/P02_VowelsCount/P02_VowelsCount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace P02_VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+
+            Console.WriteLine(VowelsConter(input));
+        }
+
+        private static int VowelsConter(string input)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a'
+                    || input[i] == 'e'
+                    || input[i] == 'u'
+                    || input[i] == 'i'
+                    || input[i] == 'o')
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
