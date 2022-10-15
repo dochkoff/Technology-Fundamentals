2c9c99bc7098c70f139bd47c76d951c6aa2869e7
dochkoff <hounds.07cinder@icloud.com>
2022-10-15 22:41:01 +0300

    T15-P01

---
+++ Technology Fundamentals with C# - 2022/T15_Methods_Exercise/Exercise/P01_SmallestOfThreeNumbers/P01_SmallestOfThreeNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(SmallestNumber(num1, num2, num3));
+        }
+
+        private static int SmallestNumber(int num1, int num2, int num3)
+        {
+            int numMin = int.MaxValue;
+
+            if (num1 < numMin)
+            {
+                numMin = num1;
+            }
+
+            if (num2 < numMin)
+            {
+                numMin = num2;
+            }
+
+            if (num3 < numMin)
+            {
+                numMin = num3;
+            }
+
+            return numMin;
+        }
+    }
+}
