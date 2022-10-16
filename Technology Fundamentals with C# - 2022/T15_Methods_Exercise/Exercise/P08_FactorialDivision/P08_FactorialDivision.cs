8395a02f5e2dab716e3da646c7ef085a34020ce5
dochkoff <hounds.07cinder@icloud.com>
2022-10-17 00:00:36 +0300

    T15-P08

---
+++ Technology Fundamentals with C# - 2022/T15_Methods_Exercise/Exercise/P08_FactorialDivision/P08_FactorialDivision.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P08_FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            double factNum1 = Factorial(num1);
+            double factNum2 = Factorial(num2);
+            double result = factNum1 / factNum2;
+
+            Console.WriteLine($"{result:F2}");
+        }
+
+        static double Factorial(double number)
+        {
+            double fact = 1;
+
+
+            for (double i = number; i > 0; i--)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+    }
+}
