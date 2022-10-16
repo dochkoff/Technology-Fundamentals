e5779ab8f9b3113f47d710781b535e0ecb2300c8
dochkoff <hounds.07cinder@icloud.com>
2022-10-16 23:20:54 +0300

    T15-P07

---
+++ Technology Fundamentals with C# - 2022/T15_Methods_Exercise/Exercise/P07_NxNMatrix/P07_NxNMatrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P07_NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            NxN(num);
+        }
+
+        private static void NxN(int num)
+        {
+            for (int r = 0; r < num; r++)
+            {
+                for (int c = 0; c < num; c++)
+                {
+                    Console.Write(num + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
