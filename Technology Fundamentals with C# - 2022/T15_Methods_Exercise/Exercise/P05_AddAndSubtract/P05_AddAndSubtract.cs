79d44fdb7b35a57f79ca21c624eb42cf9aabedc4
dochkoff <hounds.07cinder@icloud.com>
2022-10-16 22:50:01 +0300

    T15-P05

---
+++ Technology Fundamentals with C# - 2022/T15_Methods_Exercise/Exercise/P05_AddAndSubtract/P05_AddAndSubtract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace P05_AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int result = SubtractLastNumFromSum(num1, num2, num3);
+            Console.WriteLine(result);
+        }
+
+        private static int SubtractLastNumFromSum(int num1, int num2, int num3)
+        {
+            int subtract = SumFirstTwoNums(num1, num2) - num3;
+            return subtract;
+        }
+
+        private static int SumFirstTwoNums(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+    }
+}
