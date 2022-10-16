790330bf79a6db3cb7922dfdc64ebfd148afb94e
dochkoff <hounds.07cinder@icloud.com>
2022-10-16 22:40:40 +0300

    T15-P04

---
+++ Technology Fundamentals with C# - 2022/T15_Methods_Exercise/Exercise/P04_PasswordValidator/P04_PasswordValidator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_PasswordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            if (CheckPasswordLenght(password)
+                && CheckOnlyLettersAndDigits(password)
+                && CheckAtLeast2Digits(password))
+            {
+                Console.WriteLine("Password is valid");
+            }
+            else
+            {
+                if (!CheckPasswordLenght(password))
+                {
+                    Console.WriteLine("Password must be between 6 and 10 characters");
+                }
+
+                if (!CheckOnlyLettersAndDigits(password))
+                {
+                    Console.WriteLine("Password must consist only of letters and digits");
+                }
+
+                if (!CheckAtLeast2Digits(password))
+                {
+                    Console.WriteLine("Password must have at least 2 digits");
+                }
+            }
+        }
+        private static bool CheckPasswordLenght(string password)
+        {
+            bool passLenght = password.Length >= 6 && password.Length <= 10;
+            return passLenght;
+
+        }
+
+        private static bool CheckOnlyLettersAndDigits(string password)
+        {
+            foreach (char c in password)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+
+        private static bool CheckAtLeast2Digits(string password)
+        {
+            int counter = 0;
+
+            foreach (char c in password)
+            {
+                if (char.IsDigit(c))
+                {
+                    counter++;
+                }
+            }
+
+            if (counter >= 2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
