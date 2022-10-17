07b8fe4ee861be9950f3d31d70457c5db6d7899e
dochkoff <hounds.07cinder@icloud.com>
2022-10-17 20:39:52 +0300

    T15-P09

---
+++ Technology Fundamentals with C# - 2022/T15_Methods_Exercise/Exercise/P09_PalindromeIntegers/P09_PalindromeIntegers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P09_PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                Palindrome(input);
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void Palindrome(string input)
+        {
+            int[] numbers = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                numbers[i] = input[i] - '0';
+            }
+
+            if (numbers[0] == numbers[numbers.Length - 1])
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
