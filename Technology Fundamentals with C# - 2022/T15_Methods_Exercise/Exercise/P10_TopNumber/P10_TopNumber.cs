bddf5863e17df4b866a451e1c95c3617119c78b5
dochkoff <hounds.07cinder@icloud.com>
2022-10-17 21:47:25 +0300

    T15-P10

---
+++ Technology Fundamentals with C# - 2022/T15_Methods_Exercise/Exercise/P10_TopNumber/P10_TopNumber.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace P10_TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lastNumberRange = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < lastNumberRange; i++)
+            {
+                string number = i.ToString();
+
+                int[] numbers = new int[number.Length];
+
+                for (int k = 0; k < number.Length; k++)
+                {
+                    numbers[k] = number[k] - '0';
+                }
+
+                if (DevideByEight(numbers) && LeastOneOdd(numbers))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static bool LeastOneOdd(int[] numbers)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool DevideByEight(int[] numbers)
+        {
+            int numbersSum = numbers.Sum();
+
+            if (numbersSum % 8 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
