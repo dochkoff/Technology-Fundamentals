3fe62fa0d868af896b170fd4dbbc66c669a7e8d7
dochkoff <hounds.07cinder@icloud.com>
2022-10-18 09:33:56 +0300

    T17-P02

---
+++ Technology Fundamentals with C# - 2022/T17_List/P02_GaussTrick/P02_GaussTrick.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> gaussTrick = new List<int>();
+
+            if (numbers.Count % 2 == 0)
+            {
+                GaussTrick(ref numbers, gaussTrick);
+            }
+            else
+            {
+                GaussTrick(ref numbers, gaussTrick);
+
+                gaussTrick.Add(numbers[0]);
+            }
+
+            Console.WriteLine(string.Join(" ", gaussTrick));
+        }
+
+        private static List<int> GaussTrick(ref List<int> numbers, List<int> gaussTrick)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                gaussTrick.Add(numbers[i] + numbers[numbers.Count - 1]);
+                numbers.Remove(numbers[i]);
+                numbers.Remove(numbers[numbers.Count - 1]);
+                i = -1;
+            }
+
+            return gaussTrick;
+        }
+    }
+}
