e5783dba69c7a198b6e4bfea309842920d351197
dochkoff <hounds.07cinder@icloud.com>
2022-10-18 22:24:10 +0300

    T18-P03

---
+++ Technology Fundamentals with C# - 2022/T18_List_Exercise/Exercise/P03_HouseParty/P03_HouseParty.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            int counter = 0;
+            List<string> command = new List<string>();
+            List<string> names = new List<string>();
+
+            while (numberOfCommands >= counter)
+            {
+                command = Console.ReadLine().Split().ToList();
+
+                if (command[2] == "going!")
+                {
+                    if (names.Contains(command[0]))
+                    {
+                        Console.WriteLine($"{command[0]} is already in the list!");
+                    }
+                    else
+                    {
+                        names.Add(command[0]);
+                    }
+                }
+                else if (command[2] == "not")
+                {
+                    if (names.Contains(command[0]))
+                    {
+                        names.Remove(command[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{command[0]} is not in the list!");
+                    }
+                }
+
+                counter++;
+            }
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
+
