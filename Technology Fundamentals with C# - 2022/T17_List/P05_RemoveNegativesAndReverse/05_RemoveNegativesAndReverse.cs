ddad4d9c4b44a99441ec2d7a82b4876425f23217
dochkoff <hounds.07cinder@icloud.com>
2022-10-18 12:45:16 +0300

    T17-P05

---
+++ Technology Fundamentals with C# - 2022/T17_List/P05_RemoveNegativesAndReverse/05_RemoveNegativesAndReverse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> onlyPositiveNums = new List<int>();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] >= 0)
+                {
+                    onlyPositiveNums.Add(input[i]);
+                }
+            }
+
+            if (onlyPositiveNums.Count > 0)
+            {
+                onlyPositiveNums.Reverse();
+                Console.WriteLine(string.Join(" ", onlyPositiveNums));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
