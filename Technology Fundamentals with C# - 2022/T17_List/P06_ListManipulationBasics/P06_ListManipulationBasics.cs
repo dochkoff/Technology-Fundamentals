a81b5188f12a9aa245a293e66eec15cde1679a4f
dochkoff <hounds.07cinder@icloud.com>
2022-10-18 13:23:10 +0300

    T17-P06

---
+++ Technology Fundamentals with C# - 2022/T17_List/P06_ListManipulationBasics/P06_ListManipulationBasics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06_ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command = Console.ReadLine();
+            string[] commandSplit = command.Split().ToArray();
+
+
+            while (command != "end")
+            {
+                commandSplit = command.Split().ToArray();
+
+                switch (commandSplit[0])
+                {
+                    case "Add":
+                        input.Add(int.Parse(commandSplit[1]));
+                        break;
+                    case "Remove":
+                        input.Remove(int.Parse(commandSplit[1]));
+                        break;
+                    case "RemoveAt":
+                        input.RemoveAt(int.Parse(commandSplit[1]));
+                        break;
+                    case "Insert":
+                        input.Insert(int.Parse(commandSplit[2]), int.Parse(commandSplit[1]));
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
