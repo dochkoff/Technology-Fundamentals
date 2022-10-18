4cf291d7f8cec855484c7cbff1e6840507cdb382
dochkoff <hounds.07cinder@icloud.com>
2022-10-18 12:08:39 +0300

    T17-P03

---
+++ Technology Fundamentals with C# - 2022/T17_List/P03_MergingLists/P03_MergingLists.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstGroup = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secoundGroup = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < Math.Min(firstGroup.Count, secoundGroup.Count); i++)
+            {
+                result.Add(firstGroup[i]);
+                result.Add(secoundGroup[i]);
+            }
+
+            if (firstGroup.Count != secoundGroup.Count)
+            {
+                for (int i = Math.Min(firstGroup.Count, secoundGroup.Count) + 1; i <= LongerListCount(firstGroup, secoundGroup); i++)
+                {
+                    result.Add(LongerList(firstGroup, secoundGroup)[i - 1]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static int LongerListCount(List<int> list1, List<int> list2)
+        {
+            if (list1.Count >= list2.Count)
+            {
+                return list1.Count;
+            }
+            else
+            {
+                return list2.Count;
+            }
+        }
+
+        private static List<int> LongerList(List<int> list1, List<int> list2)
+        {
+            if (list1.Count >= list2.Count)
+            {
+                return list1;
+            }
+            else
+            {
+                return list2;
+            }
+        }
+    }
+}
