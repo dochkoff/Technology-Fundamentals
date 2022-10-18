60bf8b3d5601839e9a7efb150e51f048ec260262
dochkoff <hounds.07cinder@icloud.com>
2022-10-18 21:51:07 +0300

    T18-P02

---
+++ Technology Fundamentals with C# - 2022/T18_List_Exercise/Exercise/P02_ChangeList/P02_ChangeList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                List<string> commands = input.Split().ToList();
+
+                switch (commands[0])
+                {
+                    case "Delete":
+                        for (int i = 0; i < numberList.Count; i++)
+                        {
+                            if (numberList[i] == int.Parse(commands[1]))
+                            {
+                                numberList.Remove(int.Parse(commands[1]));
+                                i = -1;
+                            }
+                        }
+                        break;
+                    case "Insert":
+                        numberList.Insert(int.Parse(commands[2]), int.Parse(commands[1]));
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", numberList));
+        }
+    }
+}
