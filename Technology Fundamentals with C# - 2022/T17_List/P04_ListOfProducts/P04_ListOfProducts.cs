83dc2c9a39d8f473e419d3f0ff965db09ad918d5
dochkoff <hounds.07cinder@icloud.com>
2022-10-18 12:34:56 +0300

    T17-P04

---
+++ Technology Fundamentals with C# - 2022/T17_List/P04_ListOfProducts/P04_ListOfProducts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int productsCount = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < productsCount; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
