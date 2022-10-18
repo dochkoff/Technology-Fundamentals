03b249a62311cdac1c3cc2b156bf5e88889d04a5
dochkoff <hounds.07cinder@icloud.com>
2022-10-18 21:38:41 +0300

    T18-P01

---
+++ Technology Fundamentals with C# - 2022/T18_List_Exercise/Exercise/P01_Train/P01_Train.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int maxCapacity = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                List<string> command = input.Split().ToList();
+
+                if (command[0] == "Add")
+                {
+                    wagons.Add(int.Parse(command[1]));
+                }
+                else if (command.Count == 1)
+                {
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + int.Parse(command[0]) <= maxCapacity)
+                        {
+                            wagons[i] += int.Parse(command[0]);
+                            break;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
