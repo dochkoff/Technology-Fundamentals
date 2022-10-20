9561f4faf8ed3db0b03e9192554219d3d17a7c5f
dochkoff <hounds.07cinder@icloud.com>
2022-10-21 00:35:48 +0300

    T19-P03

---
+++ Technology Fundamentals with C# - 2022/T19_MidExamPreparation/P03_MovingTarget/P03_MovingTarget.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> target = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+        string input = Console.ReadLine();
+
+        while (input != "End")
+        {
+            string[] command = input.Split().ToArray();
+
+            int index = int.Parse(command[1]);
+            int value = int.Parse(command[2]);
+
+            if (command[0] == "Shoot")
+            {
+                if (index >= 0 && index < target.Count)
+                {
+                    target[index] -= value;
+
+                    if (target[index] <= 0)
+                    {
+                        target.RemoveAt(index);
+                    }
+                }
+            }
+            else if (command[0] == "Add")
+            {
+                if (index >= 0 && index < target.Count)
+                {
+                    target.Insert(index, value);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid placement!");
+                }
+            }
+            else if (command[0] == "Strike")
+            {
+                if (index - value >= 0 && index + value < target.Count)
+                {
+                    target.RemoveRange(index - value, value * 2 + 1);
+                }
+                else
+                {
+                    Console.WriteLine("Strike missed!");
+                }
+            }
+
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine(string.Join("|", target));
+    }
+}
