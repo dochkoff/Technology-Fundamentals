b9b3ecd3d04666301a9ca0ade7bfa924000e379e
dochkoff <hounds.07cinder@icloud.com>
2022-10-20 21:15:00 +0300

    T19-P02

---
+++ Technology Fundamentals with C# - 2022/T19_MidExamPreparation/P02_ShoppingList/P02_ShoppingList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int passangers = int.Parse(Console.ReadLine());
+            List<int> wagon = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int i = 0; i < wagon.Count; i++)
+            {
+                while (wagon[i] < 4 && passangers > 0)
+                {
+                    wagon[i] += 1;
+                    passangers--;
+                }
+            }
+
+            if (passangers == 0 && wagon.Any(x => x < 4))
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", wagon));
+            }
+            else if (passangers > 0 && wagon.All(x => x == 4))
+            {
+                Console.WriteLine($"There isn't enough space! {passangers} people in a queue!");
+                Console.WriteLine(string.Join(" ", wagon));
+            }
+            else if (passangers == 0 && wagon.All(x => x == 4))
+            {
+                Console.WriteLine(string.Join(" ", wagon));
+            }
+        }
+    }
+}
