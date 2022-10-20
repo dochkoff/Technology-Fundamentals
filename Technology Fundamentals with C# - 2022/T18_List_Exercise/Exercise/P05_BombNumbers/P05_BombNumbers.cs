21dad17a7e2d2c2fa3583f84f32a32cb95641384
dochkoff <hounds.07cinder@icloud.com>
2022-10-20 11:39:34 +0300

    T19-P01

--- Technology Fundamentals with C# - 2022/T18_List_Exercise/Exercise/P05_BombNumbers/P05_BombNumbers.cs
+++ Technology Fundamentals with C# - 2022/T18_List_Exercise/Exercise/P05_BombNumbers/P05_BombNumbers.cs
@@ -11,43 +11,79 @@ namespace P05_BombNumbers
             List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
             List<int> bombAndPower = Console.ReadLine().Split().Select(int.Parse).ToList();
 
-            int bomb = bombAndPower[0];
-            int power = bombAndPower[1];
-            int detRangeIndx = 2 * power + 1;
-            int detStartIndx = 0;
-            int detEndIndx = 0;
-
-            for (int bombIndex = 0; bombIndex < numbers.Count; bombIndex++)
+            for (int i = 0; i < numbers.Count; i++)
             {
-                if (numbers[bombIndex] == bomb)
+                if (numbers[i] == bombAndPower[0])
                 {
-                    detStartIndx = bombIndex - power;
-                    detEndIndx = bombIndex + power;
+                    for (int j = 1; j <= bombAndPower[1]; j++)
+                    {
+                        if (i - j < 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            numbers[i - j] = 0;
+                        }
+                    }
 
-                    //no numbers left
-                    if (detStartIndx < 0 && detEndIndx <= numbers.Count - 1)
+                    for (int j = 1; j <= bombAndPower[1]; j++)
                     {
-                        numbers.RemoveRange(0, detEndIndx + 1);
+                        if (i + j > numbers.Count - 1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            numbers[i + j] = 0;
+                        }
                     }
-                    //no numbers right
-                    else if (detEndIndx > numbers.Count - 1 && detStartIndx >= 0)
-                    {
-                        numbers.RemoveRange(detStartIndx, (power + 1) + (numbers.Count - 1) - bombIndex);
-                    }
-                    //no numbers left & right
-                    else if (detStartIndx < 0 && detEndIndx > numbers.Count - 1)
-                    {
-                        numbers.RemoveRange(0, numbers.Count);
-                    }
-                    //in range
-                    else
-                    {
-                        numbers.RemoveRange(detStartIndx, detRangeIndx);
-                    }
+
+                    numbers[i] = 0;
                 }
+
             }
 
             Console.WriteLine(numbers.Sum());
+
+            //My variant (75/100 in Judge)
+            //int bomb = bombAndPower[0];
+            //int power = bombAndPower[1];
+            //int detRangeIndx = 2 * power + 1;
+            //int detStartIndx = 0;
+            //int detEndIndx = 0;
+
+            //for (int bombIndex = 0; bombIndex < numbers.Count; bombIndex++)
+            //{
+            //    if (numbers[bombIndex] == bomb)
+            //    {
+            //        detStartIndx = bombIndex - power;
+            //        detEndIndx = bombIndex + power;
+
+            //        //no numbers left
+            //        if (detStartIndx < 0 && detEndIndx <= numbers.Count - 1)
+            //        {
+            //            numbers.RemoveRange(0, detEndIndx + 1);
+            //        }
+            //        //no numbers right
+            //        else if (detEndIndx > numbers.Count - 1 && detStartIndx >= 0)
+            //        {
+            //            numbers.RemoveRange(detStartIndx, (power + 1) + (numbers.Count - 1) - bombIndex);
+            //        }
+            //        //no numbers left & right
+            //        else if (detStartIndx < 0 && detEndIndx > numbers.Count - 1)
+            //        {
+            //            numbers.RemoveRange(0, numbers.Count);
+            //        }
+            //        //in range
+            //        else
+            //        {
+            //            numbers.RemoveRange(detStartIndx, detRangeIndx);
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine(numbers.Sum());
         }
     }
 }
---
+++ Technology Fundamentals with C# - 2022/T19_MidExamPreparation/P01_GuineaPig/P01_GuineaPig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace P01_GuineaPig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal food = decimal.Parse(Console.ReadLine()); //kg
+            decimal hay = decimal.Parse(Console.ReadLine()); //kg
+            decimal cover = decimal.Parse(Console.ReadLine()); //kg
+            decimal weight = decimal.Parse(Console.ReadLine()); //kg
+
+            for (int day = 1; day <= 30; day++)
+            {
+                food -= 0.3m;
+
+                if (day % 2 == 0)
+                {
+                    hay -= food * 0.05m;
+                }
+
+                if (day % 3 == 0)
+                {
+                    cover -= weight / 3.0m;
+                }
+
+                if (food <= 0
+                    || hay <= 0
+                    || cover <= 0)
+                {
+                    Console.WriteLine("Merry must go to the pet store!");
+                    break;
+                }
+
+                if (day == 30)
+                {
+                    Console.WriteLine($"Everything is fine! Puppy is happy! Food: {food:F2}, Hay: {hay:F2}, Cover: {cover:F2}.");
+                }
+            }
+        }
+    }
+}
