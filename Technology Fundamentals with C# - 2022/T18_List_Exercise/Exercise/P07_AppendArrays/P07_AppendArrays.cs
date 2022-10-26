66cb4668fde90f203eed0680de6dfc4cc1c30afa
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-10-26 17:07:44 +0300

    T18-P07

---
+++ Technology Fundamentals with C# - 2022/T18_List_Exercise/Exercise/P07_AppendArrays/P07_AppendArrays.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P07_AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .Split('|')
+                .ToList();
+            input.Reverse();
+            string fromRightToLeft = string.Join(" ", input);
+            List<int> appendList = fromRightToLeft
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Console.WriteLine(string.Join(" ", appendList));
+        }
+    }
+}
