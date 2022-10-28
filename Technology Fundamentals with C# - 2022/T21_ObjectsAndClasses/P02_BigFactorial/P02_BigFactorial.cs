92242d41cfddf6cdd67155dc1e8801148ef74c0f
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-10-28 23:28:36 +0300

    T21-P02

---
+++ Technology Fundamentals with C# - 2022/T21_ObjectsAndClasses/P02_BigFactorial/P02_BigFactorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace P02_BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+
+            for (int i = 2; i <= inputNumber; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
