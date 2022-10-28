6d10890d01d20439b0b70ff240088a908803184d
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-10-28 22:07:01 +0300

    T21-P01

--- Technology Fundamentals with C# - 2022/T21_ObjectsAndClasses/P01_RandomizeWords/P01_RandomizeWords.cs
+++ Technology Fundamentals with C# - 2022/T21_ObjectsAndClasses/P01_RandomizeWords/P01_RandomizeWords.cs
@@ -8,7 +8,23 @@ namespace P01_RandomizeWords
     {
         static void Main(string[] args)
         {
-            
+            string[] input = Console.ReadLine()
+                .Split();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int randomIndex = rnd.Next(0, input.Length);
+
+                string currentWord = input[i];
+                string randomWord = input[randomIndex];
+
+                input[i] = randomWord;
+                input[randomIndex] = currentWord;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, input));
         }
     }
 }
