d4cffeb51f49086404596ffdbcda2cd790610542
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-10-29 19:37:22 +0300

    T21-P05

---
+++ Technology Fundamentals with C# - 2022/T21_ObjectsAndClasses/P05_Students2.0/P05_Students2.0.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P04_Students2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Student> students = new List<Student>();
+
+            while (input != "end")
+            {
+                string[] studentInfo = input.Split();
+
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                int age = int.Parse(studentInfo[2]);
+                string city = studentInfo[3];
+
+                if (IsStudentExisting(students, firstName, lastName))
+                {
+                    Student student = GetStudent(students, firstName, lastName);
+
+                    student.FirstName = firstName;
+                    student.LastName = lastName;
+                    student.Age = age;
+                    student.City = city;
+                }
+                else
+                {
+                    Student student = new Student(firstName, lastName, age, city);
+                    students.Add(student);
+                }
+
+
+
+                input = Console.ReadLine();
+            }
+
+            string desiredCity = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.City == desiredCity)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+
+        static Student GetStudent(List<Student> students, string firstName, string lastName)
+        {
+            Student existingStudent = null;
+
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    existingStudent = student;
+                }
+            }
+
+            return existingStudent;
+        }
+
+        static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
+        {
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, int age, string city)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            City = city;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
