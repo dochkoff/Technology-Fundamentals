224e6514f289cb3f4e563221106d4f86f9f37d68
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-02 23:58:01 +0200

    T22-P07

---
+++ Technology Fundamentals with C# - 2022/T22_ObjectsAndClasses_Exercise/Exercise/P07_OrderByAge/P07_OrderByAge.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P07_OrderByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var persons = new List<Person>();
+
+            while (input != "End")
+            {
+                string[] personInfo = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personInfo[0];
+                string personID = personInfo[1];
+                int age = int.Parse(personInfo[2]);
+
+                Person person = new Person(name, personID, age);
+                persons.Add(person);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, string personID, int age)
+        {
+            Name = name;
+            PersonID = personID;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public string PersonID { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} with ID: {this.PersonID} is {Age} years old.";
+        }
+    }
+}
