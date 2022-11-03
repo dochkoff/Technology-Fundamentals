b62620a58fabcfa8e41883b4437f964f1f3732c4
dochkoff <hounds.07cinder@icloud.com>
2022-11-03 23:44:51 +0200

    T24-P04

---
+++ Technology Fundamentals with C# - 2022/T24_AssociativeArrays/P04_WordFilter/P04_WordFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace P04_WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split()
+                .Where(x => x.Length % 2 == 0)
+                .ToList()
+                .ForEach(Console.WriteLine);
+
+            //string[] input = Console.ReadLine().Split();
+
+            //foreach (var word in input)
+            //{
+            //    if (word.Length%2==0)
+            //    {
+            //        Console.WriteLine(word);
+            //    }
+            //}
+        }
+    }
+}
