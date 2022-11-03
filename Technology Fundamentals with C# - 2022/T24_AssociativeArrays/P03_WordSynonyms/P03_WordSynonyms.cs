f89eabbfe8478e5fa374b177fd3b394361f725fc
dochkoff <hounds.07cinder@icloud.com>
2022-11-03 23:21:34 +0200

    T24-P03

---
+++ Technology Fundamentals with C# - 2022/T24_AssociativeArrays/P03_WordSynonyms/P03_WordSynonyms.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03_WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeats = int.Parse(Console.ReadLine());
+
+            var wordDict = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < repeats; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!wordDict.ContainsKey(word))
+                {
+                    wordDict.Add(word, new List<string>());
+                }
+
+                wordDict[word].Add(synonym);
+            }
+
+            foreach (var word in wordDict)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
