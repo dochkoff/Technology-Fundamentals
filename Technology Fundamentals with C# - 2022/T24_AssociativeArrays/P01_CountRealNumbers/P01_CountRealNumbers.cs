6ce4ff92933125f40fe8a491f070eb9b35403977
dochkoff <hounds.07cinder@icloud.com>
2022-11-03 22:51:23 +0200

    T24-P01

---
+++ Technology Fundamentals with C# - 2022/T24_AssociativeArrays/P01_CountRealNumbers/P01_CountRealNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P01_CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            var sortedNums = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (sortedNums.ContainsKey(number))
+                {
+                    sortedNums[number]++;
+                }
+                else
+                {
+                    sortedNums.Add(number, 1);
+                }
+            }
+
+            foreach (var number in sortedNums)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
