4bae75bb7356efe1a0aa8194bb6f6e2cf7fef854
dochkoff <hounds.07cinder@icloud.com>
2022-11-03 22:53:42 +0200

    T24-P02

---
+++ Technology Fundamentals with C# - 2022/T24_AssociativeArrays/P02_OddOccurrences/P02_OddOccurrences.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().ToLower().Split();
+
+            var wordsDic = new Dictionary<string, int>();
+
+            foreach (var word in input)
+            {
+                if (wordsDic.ContainsKey(word))
+                {
+                    wordsDic[word]++;
+                }
+                else
+                {
+                    wordsDic.Add(word, 1);
+                }
+            }
+
+            foreach (var word in wordsDic)
+            {
+                if (word.Value % 2 != 0)
+                {
+                    Console.Write($"{word.Key} ");
+                }
+            }
+        }
+    }
+}
