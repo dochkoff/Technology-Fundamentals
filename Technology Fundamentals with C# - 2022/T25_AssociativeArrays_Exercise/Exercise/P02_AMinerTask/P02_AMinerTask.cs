30638c1cee7409d1604730d3b7a884e983a45d22
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-06 22:48:13 +0200

    T25-P02

---
+++ Technology Fundamentals with C# - 2022/T25_AssociativeArrays_Exercise/Exercise/P02_AMinerTask/P02_AMinerTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P02_AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var preciousMetals = new Dictionary<string, int>();
+
+            string metal;
+            while ((metal = Console.ReadLine()) != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!preciousMetals.ContainsKey(metal))
+                {
+                    preciousMetals[metal] = 0;
+                }
+                preciousMetals[metal] += quantity;
+            }
+
+            foreach (var kvp in preciousMetals)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
