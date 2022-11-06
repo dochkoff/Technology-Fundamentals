efc381bf57eca93236cbfddc3e588edce508f4d9
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-06 22:38:19 +0200

    T25-P01

---
+++ Technology Fundamentals with C# - 2022/T25_AssociativeArrays_Exercise/Exercise/P01_CountCharsInAString/P01_CountCharsInAString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P01_CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var charsCount = new Dictionary<char, int>();
+
+            string input = Console.ReadLine();
+
+            foreach (char ch in input)
+            {
+                if (ch != ' ')
+                {
+                    if (!charsCount.ContainsKey(ch))
+                    {
+                        charsCount[ch] = 0;
+                    }
+                    charsCount[ch]++;
+                }
+            }
+
+            foreach (var kvp in charsCount)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
