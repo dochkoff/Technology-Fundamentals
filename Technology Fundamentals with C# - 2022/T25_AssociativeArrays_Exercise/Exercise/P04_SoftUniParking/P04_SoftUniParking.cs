8ee6ff442541a83ab85cbc226794416457ca718d
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-07 23:00:54 +0200

    T25-P04

---
+++ Technology Fundamentals with C# - 2022/T25_AssociativeArrays_Exercise/Exercise/P04_SoftUniParking/P04_SoftUniParking.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P04_SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parkingLots = new Dictionary<string, string>();
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = input[0];
+                string username = input[1];
+
+                if (command == "register")
+                {
+                    string licensePlateNumber = input[2];
+
+                    if (parkingLots.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                    else
+                    {
+                        parkingLots[username] = licensePlateNumber;
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                }
+                else if (command == "unregister")
+                {
+                    if (!parkingLots.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        parkingLots.Remove(username);
+
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var kvp in parkingLots)
+            {
+                Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+            }
+        }
+    }
+}
