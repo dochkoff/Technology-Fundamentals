231a6e15242e47be59a63d2dab460b4b4806f12c
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-07 20:46:39 +0200

    T25-P03

---
+++ Technology Fundamentals with C# - 2022/T25_AssociativeArrays_Exercise/Exercise/P03_Orders/P03_Orders.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P03_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orders = new Dictionary<string, List<double>>();
+
+            string order;
+            while ((order = Console.ReadLine()) != "buy")
+            {
+                string[] orderInfo = order.Split();
+
+                string productName = orderInfo[0];
+                double productPrice = double.Parse(orderInfo[1]);
+                double productQuantity = double.Parse(orderInfo[2]);
+
+                if (!orders.ContainsKey(productName))
+                {
+                    orders[productName] = new List<double>();
+                    orders[productName].Add(productPrice);
+                    orders[productName].Add(productQuantity);
+                }
+                else
+                {
+                    if (productPrice != orders[productName][0])
+                    {
+                        orders[productName][0] = productPrice;
+                    }
+                    orders[productName][1] += productQuantity;
+                }
+            }
+
+            foreach (var kvp in orders)
+            {
+                Console.WriteLine($"{kvp.Key} -> {(kvp.Value[0] * kvp.Value[1]):F2}");
+            }
+        }
+    }
+}
