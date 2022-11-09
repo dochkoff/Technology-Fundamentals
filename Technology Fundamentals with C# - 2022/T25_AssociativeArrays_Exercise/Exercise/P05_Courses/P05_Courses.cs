3f2605f06f438d299f9b54d655dded48722a5751
dochkoff <hounds.07cinder@icloud.com>
2022-11-09 11:25:41 +0200

    T25-P05

---
+++ Technology Fundamentals with C# - 2022/T25_AssociativeArrays_Exercise/Exercise/P05_Courses/P05_Courses.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var courses = new Dictionary<string, List<string>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] courseInfo = command
+                    .Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+                string courseName = courseInfo[0];
+                string studentName = courseInfo[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses[courseName] = new List<string>();
+                }
+
+                courses[courseName].Add(studentName);
+            }
+
+            foreach (var kvp in courses)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count()}");
+
+                foreach (var student in kvp.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
