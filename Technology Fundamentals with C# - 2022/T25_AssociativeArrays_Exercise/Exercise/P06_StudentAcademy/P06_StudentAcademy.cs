c4853d69ed9e8790cfe2d48e1fc352bcf8438f31
dochkoff <hounds.07cinder@icloud.com>
2022-11-09 11:45:06 +0200

    T25-P06

---
+++ Technology Fundamentals with C# - 2022/T25_AssociativeArrays_Exercise/Exercise/P06_StudentAcademy/P06_StudentAcademy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06_StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentAcademy = new Dictionary<string, List<double>>();
+            int numberOfEntries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (!studentAcademy.ContainsKey(studentName))
+                {
+                    studentAcademy[studentName] = new List<double>();
+                }
+
+                studentAcademy[studentName].Add(studentGrade);
+            }
+
+            foreach (var kvp in studentAcademy)
+            {
+                if (kvp.Value.Average() >= 4.5)
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value.Average():F2}");
+                }
+            }
+        }
+    }
+}
