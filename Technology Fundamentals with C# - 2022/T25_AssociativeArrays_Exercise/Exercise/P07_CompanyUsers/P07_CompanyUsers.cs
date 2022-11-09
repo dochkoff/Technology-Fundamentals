3eaf6d29615ebe540f05135d683c5b85812a28d8
dochkoff <hounds.07cinder@icloud.com>
2022-11-09 12:07:51 +0200

    T25-P07

---
+++ Technology Fundamentals with C# - 2022/T25_AssociativeArrays_Exercise/Exercise/P07_CompanyUsers/P07_CompanyUsers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07_CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var companies = new Dictionary<string, List<string>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] companyInfo = command
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string companyName = companyInfo[0];
+                string employeeID = companyInfo[1];
+
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies[companyName] = new List<string>();
+                }
+
+                if (!companies[companyName].Contains(employeeID))
+                {
+                    companies[companyName].Add(employeeID);
+                }
+            }
+
+            foreach (var kvp in companies)
+            {
+                Console.WriteLine($"{kvp.Key}");
+
+                foreach (var employee in kvp.Value)
+                {
+                    Console.WriteLine($"-- {employee}");
+                }
+            }
+        }
+    }
+}
