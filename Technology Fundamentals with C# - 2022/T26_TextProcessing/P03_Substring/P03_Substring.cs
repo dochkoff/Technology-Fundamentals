49e4b2f720d7f2a95c4e9af0d879b2c516ba50bb
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-10 23:27:43 +0200

    T26-P03

---
+++ Technology Fundamentals with C# - 2022/T26_TextProcessing/P03_Substring/P03_Substring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P03_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string occurrencess = Console.ReadLine();
+
+            while (occurrencess.Contains(word))
+            {
+                int index = occurrencess.IndexOf(word);
+                occurrencess = occurrencess.Remove(index, word.Length);
+            }
+
+            Console.WriteLine(occurrencess);
+        }
+    }
+}
