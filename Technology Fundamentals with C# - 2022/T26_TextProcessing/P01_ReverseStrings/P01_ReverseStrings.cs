e4bade1492aa1142054a3401189a5dab503f5a9e
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-10 23:27:08 +0200

    T26-P01

---
+++ Technology Fundamentals with C# - 2022/T26_TextProcessing/P01_ReverseStrings/P01_ReverseStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P01_ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string line;
+
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string reversedLine = "";
+
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    reversedLine += line[i];
+                }
+
+                Console.WriteLine($"{line} = {reversedLine}");
+            }
+
+        }
+    }
+}
