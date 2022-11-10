fb628737f356e63710de69cad83439cb21b417a6
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-10 23:27:27 +0200

    T26-P02

---
+++ Technology Fundamentals with C# - 2022/T26_TextProcessing/P02_RepeatStrings/P02_RepeatStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace P02_RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var word in input)
+            {
+                int count = word.Length;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result.Append(word);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
