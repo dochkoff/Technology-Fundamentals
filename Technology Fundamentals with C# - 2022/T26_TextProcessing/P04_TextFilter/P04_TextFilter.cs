98de80272419f0a1fac0e98bad322a3b7ea8a9f9
dochkoff <hounds.07cinder@icloud.com>
2022-11-11 15:53:00 +0200

    T26-P04

---
+++ Technology Fundamentals with C# - 2022/T26_TextProcessing/P04_TextFilter/P04_TextFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var currBanWord in bannedWords)
+            {
+                text.Replace(currBanWord, new string('*', currBanWord.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
