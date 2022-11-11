cf4afa2c1c287a3521c6c58a5e0260ba71dc3596
dochkoff <hounds.07cinder@icloud.com>
2022-11-11 16:06:39 +0200

    T26-P05

---
+++ Technology Fundamentals with C# - 2022/T26_TextProcessing/T05_DigitsLettersAndOthers/T05_DigitsLettersAndOthers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05_DigitsLettersAndOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string digits = string.Empty;
+            string letters = string.Empty;
+            string others = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    digits += input[i];
+                }
+
+                if (char.IsLetter(input[i]))
+                {
+                    letters += input[i];
+                }
+
+                if (!char.IsLetterOrDigit(input[i]))
+                {
+                    others += input[i];
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
