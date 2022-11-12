41cbd62d0f9ebbfd1380cb67abfd8eb733bcd300
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-12 15:29:30 +0200

    T27-P05

---
+++ Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P05_OddTimes/P05_OddTimes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace P05_OddTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int result = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result = result ^ numbers[i];
+            }
+
+            Console.WriteLine(result);
+
+            //example with first input:
+            //result=numbers[0]=1=001;
+            //result=010^001=011;
+            //result=011^011=000;
+            //result=010^000=010;
+            //result=011^010=001;
+            //result=001^001=000;
+            //result=011^000=011=3;
+        }
+    }
+}
+
