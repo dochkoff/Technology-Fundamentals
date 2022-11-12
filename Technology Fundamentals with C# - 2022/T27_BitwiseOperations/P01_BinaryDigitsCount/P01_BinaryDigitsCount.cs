bfdd2d839e9afbbe2b1f8f649f202d2cf1e9fd86
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-12 13:41:16 +0200

    T27-P01

---
+++ Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P01_BinaryDigitsCount/P01_BinaryDigitsCount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P01_BinaryDigitsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int binarydigit = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            while (number > 0)
+            {
+                if (number % 2 == binarydigit)
+                {
+                    counter++;
+                }
+
+                number /= 2;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
