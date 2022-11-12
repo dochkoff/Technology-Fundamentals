8cbe19b0b97e1791c99fcb00ffdc76276cb65c33
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-12 15:42:43 +0200

    Update P03_PthBit.cs

--- Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P03_PthBit/P03_PthBit.cs
+++ Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P03_PthBit/P03_PthBit.cs
@@ -9,12 +9,17 @@ namespace P03_PthBit
             int number = int.Parse(Console.ReadLine());
             int position = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < position; i++)
-            {
-                number /= 2;
-            }
+            //Bitwise
+            int mask = 1 << position;//after shift left with position value we have 1 value in mask at current position but another are 0
+            int result = (number & mask) >> position;//after shift right with position value we have 0 or 1 at last position but another are 0 using &
+            Console.WriteLine(result);
 
-            Console.WriteLine(number % 2);
+            //No bitwise
+            //for (int i = 0; i < position; i++)
+            //{
+            //    number /= 2;
+            //}
+            //Console.WriteLine(number % 2);
         }
     }
 }
