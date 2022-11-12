186a64149e1a72b1e32ba3d6022df3395641af2f
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-12 13:53:23 +0200

    T27-P03

---
+++ Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P03_PthBit/P03_PthBit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P03_PthBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < position; i++)
+            {
+                number /= 2;
+            }
+
+            Console.WriteLine(number % 2);
+        }
+    }
+}
