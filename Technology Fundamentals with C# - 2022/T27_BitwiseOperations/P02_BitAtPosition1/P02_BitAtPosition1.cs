c233e33347b234eed121e6d0112cd30c2a9bdb50
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-12 13:41:25 +0200

    T27-P02

---
+++ Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P02_BitAtPosition1/P02_BitAtPosition1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P02_BitAtPosition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine()) / 2 % 2;
+            Console.WriteLine(number);
+        }
+    }
+}
