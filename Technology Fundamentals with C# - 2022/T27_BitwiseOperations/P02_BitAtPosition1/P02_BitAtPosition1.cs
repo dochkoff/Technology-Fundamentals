a0357555a348be358309a74d081830e43168a4ad
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-12 15:41:34 +0200

    Update P02_BitAtPosition1.cs

--- Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P02_BitAtPosition1/P02_BitAtPosition1.cs
+++ Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P02_BitAtPosition1/P02_BitAtPosition1.cs
@@ -6,8 +6,19 @@ namespace P02_BitAtPosition1
     {
         static void Main(string[] args)
         {
-            int number = int.Parse(Console.ReadLine()) / 2 % 2;
-            Console.WriteLine(number);
+            int number = int.Parse(Console.ReadLine());
+            int mask = 1 << 1;
+            //after shift left with 1 we have 1 value in mask at position 1 another are 0
+
+            int result = (number & mask) >> 1;
+            //after shift right with 1 taken last value(0 or 1) because another are 0 using &
+
+            Console.WriteLine(result);
+
+
+            //No Bitwise
+            //int number = int.Parse(Console.ReadLine()) / 2 % 2;
+            //Console.WriteLine(number);
         }
     }
 }
