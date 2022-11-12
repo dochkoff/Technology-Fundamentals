f35b93e9c31550551f7141414f7fd8d0f0c6cda1
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-12 15:45:03 +0200

    Update P04_BitDestroyer.cs

--- Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P04_BitDestroyer/P04_BitDestroyer.cs
+++ Technology Fundamentals with C# - 2022/T27_BitwiseOperations/P04_BitDestroyer/P04_BitDestroyer.cs
@@ -6,31 +6,40 @@ namespace P04_BitDestroyer
     {
         static void Main(string[] args)
         {
-            int inputNumber = int.Parse(Console.ReadLine());
-            int bitPosion = int.Parse(Console.ReadLine());
+            //Bitwise
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
 
-            double result = 0;
-            var inputNumberBinary = new int[12];
-
-            for (int i = 0; i < inputNumberBinary.Length; i++)
-            {
-                inputNumberBinary[i] = inputNumber % 2;
-                inputNumber /= 2;
-
-                if (inputNumber <= 0)
-                {
-                    break;
-                }
-            }
-
-            inputNumberBinary[bitPosion] = 0;
-
-            for (int i = inputNumberBinary.Length - 1; i >= 0; i--)
-            {
-                result += inputNumberBinary[i] * Math.Pow(2, i);
-            }
-
+            int mask = ~(1 << position);//after shift left with position we have mask with 1 value at current position and another are 0, but with ~(not) we have mask with 0 value at current position and another are 1
+            int result = (number & mask);// taken the whole number with set 0 only at current position and now we have another number
             Console.WriteLine(result);
+
+            //No bitwise
+            //int inputNumber = int.Parse(Console.ReadLine());
+            //int bitPosion = int.Parse(Console.ReadLine());
+
+            //double result = 0;
+            //var inputNumberBinary = new int[12];
+
+            //for (int i = 0; i < inputNumberBinary.Length; i++)
+            //{
+            //    inputNumberBinary[i] = inputNumber % 2;
+            //    inputNumber /= 2;
+
+            //    if (inputNumber <= 0)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //inputNumberBinary[bitPosion] = 0;
+
+            //for (int i = inputNumberBinary.Length - 1; i >= 0; i--)
+            //{
+            //    result += inputNumberBinary[i] * Math.Pow(2, i);
+            //}
+
+            //Console.WriteLine(result);
         }
     }
 }
