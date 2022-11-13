acbfefb4a95a41de9a5fbe29db424eb44999e934
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-13 18:07:26 +0200

    T28-P01

---
+++ Technology Fundamentals with C# - 2022/T28_TextProcessing_Exercise/Exercise/P01_ValidUsernames/P01_ValidUsernames.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P01_ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                if (usernames[i].Length >= 3 && usernames[i].Length <= 16)
+                {
+                    bool charIsValid = true;
+
+                    foreach (var ch in usernames[i])
+                    {
+                        if (!(char.IsLetterOrDigit(ch)
+                            || ch == '-'
+                            || ch == '_'))
+                        {
+                            charIsValid = false;
+                            break;
+                        }
+                    }
+
+                    if (charIsValid)
+                    {
+                        Console.WriteLine(usernames[i]);
+                    }
+                }
+            }
+        }
+    }
+}
