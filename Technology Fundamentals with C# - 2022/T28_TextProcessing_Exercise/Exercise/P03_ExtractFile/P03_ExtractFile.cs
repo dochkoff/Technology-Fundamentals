48df4dab7da6e7ee909e86423132499a016f2e26
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-13 19:37:48 +0200

    T28-P03

---
+++ Technology Fundamentals with C# - 2022/T28_TextProcessing_Exercise/Exercise/P03_ExtractFile/P03_ExtractFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P03_ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split("\\");
+
+            string[] nameAndExt = input[input.Length - 1]
+                .Split(".");
+
+            Console.WriteLine($"File name: {nameAndExt[0]}");
+            Console.WriteLine($"File extension: {nameAndExt[1]}");
+        }
+    }
+}
