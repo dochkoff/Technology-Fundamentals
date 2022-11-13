524f0c0284be1c2722678e5d74eea278cbe63a88
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-13 19:27:37 +0200

    T28-P02

---
+++ Technology Fundamentals with C# - 2022/T28_TextProcessing_Exercise/Exercise/P02_CharacterMultiplier/P02_CharacterMultiplier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P02_CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split();
+
+            string str1 = input[0];
+            string str2 = input[1];
+            int shortestString = Math.Min(str1.Length, str2.Length);
+            int result = 0;
+
+            for (int i = 0; i < shortestString; i++)
+            {
+                result += str1[i] * str2[i];
+            }
+
+
+            string longestString = str1;
+            if (longestString.Length < str2.Length)
+            {
+                longestString = str2;
+            }
+
+            for (int i = shortestString; i < longestString.Length; i++)
+            {
+                result += longestString[i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
