ec61b398594edd07a2da7ca403e23cfdb5f0c733
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-13 22:26:57 +0200

    T28-P04

---
+++ Technology Fundamentals with C# - 2022/T28_TextProcessing_Exercise/Exercise/P04_CaesarCipher/P04_CaesarCipher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P04_CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            foreach (var ch in input)
+            {
+                char cipher = (char)(ch + 3);
+                Console.Write(cipher);
+            }
+        }
+    }
+}
+
