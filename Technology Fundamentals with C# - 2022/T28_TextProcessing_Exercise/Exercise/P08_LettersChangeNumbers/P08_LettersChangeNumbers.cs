b0c5cc7356975fc0a9de5a89303d8249d24e68e3
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-19 01:18:05 +0200

    T28-P08

---
+++ Technology Fundamentals with C# - 2022/T28_TextProcessing_Exercise/Exercise/P08_LettersChangeNumbers/P08_LettersChangeNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace P08_LettersChangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            double sum = 0;
+
+            foreach (var item in input)
+            {
+                char fisrtLetter = item[0];
+                char lastLetter = item[^1]; //same as item.Lenght-1
+
+                string numAsAString = item[1..^1]; //this will return everything betwieen 0 and the last index;
+                double numberFromString = double.Parse(numAsAString);
+
+                if (char.IsUpper(fisrtLetter))
+                {
+                    int positonOfTheLetter = fisrtLetter - 64;
+                    numberFromString /= positonOfTheLetter;
+                }
+                else
+                {
+                    int positionOfTheLetter = fisrtLetter - 96;
+                    numberFromString *= positionOfTheLetter;
+                }
+
+                if (char.IsUpper(lastLetter))
+                {
+                    int positonOfTheLetter = lastLetter - 64;
+                    numberFromString -= positonOfTheLetter;
+                }
+                else
+                {
+                    int positionOfTheLetter = lastLetter - 96;
+                    numberFromString += positionOfTheLetter;
+                }
+
+                sum += numberFromString;
+            }
+
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
