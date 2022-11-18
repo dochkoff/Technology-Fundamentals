0b5dcdd2ad03bca19f4ac25a3d4360bddac9eeba
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-19 00:57:03 +0200

    T28-P06

---
+++ Technology Fundamentals with C# - 2022/T28_TextProcessing_Exercise/Exercise/P06_ReplaceRepeatingChars/P06_ReplaceRepeatingChars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace P06_ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] != input[i + 1])
+                {
+                    sb.Append(input[i]);
+                }
+            }
+
+            sb.Append(input[input.Length - 1]);
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
