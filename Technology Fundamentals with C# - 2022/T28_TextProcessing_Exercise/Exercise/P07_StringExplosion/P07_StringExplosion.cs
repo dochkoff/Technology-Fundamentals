dadfc703a51e2c277296d56d4df20b5667a50dae
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-19 00:57:19 +0200

    T28-P07

---
+++ Technology Fundamentals with C# - 2022/T28_TextProcessing_Exercise/Exercise/P07_StringExplosion/P07_StringExplosion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P07_StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int bomb = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (bomb > 0 && input[i] != '>')
+                {
+                    input = input.Remove(i, 1);
+                    bomb--;
+                    i--;
+                }
+                else if (input[i] == '>')
+                {
+                    bomb += int.Parse(input[i + 1].ToString());
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
