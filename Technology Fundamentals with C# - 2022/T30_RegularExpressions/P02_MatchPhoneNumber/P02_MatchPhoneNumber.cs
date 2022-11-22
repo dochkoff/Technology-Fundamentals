4c4a38ab3c96560036807f42a9fd6c6732b42ec4
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-22 22:14:53 +0200

    T30-P02

---
+++ Technology Fundamentals with C# - 2022/T30_RegularExpressions/P02_MatchPhoneNumber/P02_MatchPhoneNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P02_MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+\b359(-| )2\1\d{3}\1\d{4}\b";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchColl = regex.Matches(input);
+
+            string[] result = matchColl.Select(x => x.Value)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
