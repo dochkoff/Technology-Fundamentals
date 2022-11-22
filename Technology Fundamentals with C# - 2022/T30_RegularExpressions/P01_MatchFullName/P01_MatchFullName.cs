432e44bc473e0704d457a336b95d3fd11eb9c7eb
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-22 20:45:07 +0200

    T30-P01

---
+++ Technology Fundamentals with C# - 2022/T30_RegularExpressions/P01_MatchFullName/P01_MatchFullName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P01_MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchColl = regex.Matches(input);
+
+            foreach (Match item in matchColl)
+            {
+                Console.Write(item.Value + " ");
+            }
+        }
+    }
+}
