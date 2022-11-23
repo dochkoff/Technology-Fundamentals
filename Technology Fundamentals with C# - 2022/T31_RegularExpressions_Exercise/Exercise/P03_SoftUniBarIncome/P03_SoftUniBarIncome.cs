f55290b50815a1ec2864bc0a4d333696a0b99510
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-24 00:51:50 +0200

    T31-P03

---
+++ Technology Fundamentals with C# - 2022/T31_RegularExpressions_Exercise/Exercise/P03_SoftUniBarIncome/P03_SoftUniBarIncome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P03_SoftUniBarIncome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^\%(?<name>[A-z][a-z]+)\%[^\|\$\%\.]*?\<(?<product>\w+)\>[^\|\$\%\.]*?\|(?<count>\d+)\|[^\|\$\%\.]*?(?<price>\d+(\.\d+)?)\$[^\|\$\%\.]*?$";
+            Regex regex = new Regex(pattern);
+
+            double totalIncome = 0;
+            string input;
+            while ((input = Console.ReadLine()) != "end of shift")
+            {
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    string product = match.Groups["product"].Value;
+                    int count = int.Parse(match.Groups["count"].Value);
+                    double price = double.Parse(match.Groups["price"].Value);
+
+                    double totalPrice = count * price;
+                    totalIncome += totalPrice;
+                    Console.WriteLine($"{name}: {product} - {totalPrice:F2}");
+                }
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:F2}");
+        }
+    }
+}
+
