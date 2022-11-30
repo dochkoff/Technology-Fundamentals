12d18cd4abc5c5c0e8df450598eae89864a9e3a2
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-11-30 13:27:21 +0200

    T33-P01

---
+++ Technology Fundamentals with C# - 2022/T33_ExamPreparation/P01_SecretChat/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace P01_SecretChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Reveal")
+            {
+                string[] steps = command.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+
+                if (steps[0] == "InsertSpace")
+                {
+                    int index = int.Parse(steps[1]);
+                    message = message.Insert(index, " ");
+
+                    Console.WriteLine(message);
+                }
+                else if (steps[0] == "Reverse")
+                {
+                    string substring = steps[1];
+
+                    if (message.Contains(substring))
+                    {
+                        message = message.Remove(message.IndexOf(substring), substring.Length);
+                        message += string.Join("", substring.Reverse());
+
+                        Console.WriteLine(message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else if (steps[0] == "ChangeAll")
+                {
+                    string substring = steps[1];
+                    string replacement = steps[2];
+
+                    message = message.Replace(substring, replacement);
+
+                    Console.WriteLine(message);
+                }
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+    }
+}
