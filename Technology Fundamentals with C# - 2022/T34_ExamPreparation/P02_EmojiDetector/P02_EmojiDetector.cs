589a94c2cbd55bf41374f7d43070aacca15a4905
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-12-03 23:10:34 +0200

    T34-P02

---
+++ Technology Fundamentals with C# - 2022/T34_ExamPreparation/P02_EmojiDetector/P02_EmojiDetector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace P02_EmojiDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var dic = new Dictionary<string, int>();
+
+            BigInteger coolThreshold = 1;
+            foreach (var ch in input)
+            {
+                int num = 0;
+                if (int.TryParse(ch.ToString(), out num))
+                {
+                    coolThreshold *= num;
+                }
+            }
+
+            string pattern = @"(::|\*\*)[A-Z][a-z]{2,}(\1)";
+            var regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            int validEmojis = matches.Count;
+
+            foreach (Match match in matches)
+            {
+                string emoji = match.ToString();
+                int coolness = 1;
+
+                foreach (char ch in emoji)
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        coolness += (char)ch;
+                    }
+                }
+
+                if (coolness >= coolThreshold)
+                {
+                    dic[emoji] = coolness;
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {coolThreshold}");
+            Console.WriteLine($"{validEmojis} emojis found in the text. The cool ones are:");
+
+            foreach (var kvp in dic)
+            {
+                Console.WriteLine(kvp.Key);
+            }
+        }
+    }
+}
