b566874288fe60c6265bf860a8c17e00ed690524
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-12-04 15:21:14 +0200

    T37-P02

---
+++ Technology Fundamentals with C# - 2022/T37_RegularFinalExam/P02_BossRush/P02_BossRush.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace P02_BossRush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            string pattern = @"\|(?<name>[A-Z]{4,})\|:#(?<title>[A-Za-z]+ [A-Za-z]+)#";
+            Regex regex = new Regex(pattern);
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    string bossName = match.Groups["name"].Value;
+                    string title = match.Groups["title"].Value;
+
+                    Console.WriteLine($"{bossName}, The {title}");
+                    Console.WriteLine($">> Strength: {bossName.Length}");
+                    Console.WriteLine($">> Armor: {title.Length}");
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
+        }
+    }
+}
