43d40c4e41c76d196ae1925f33aeda9e6fee7ff7
Pavel Dochkov <hounds.07cinder@icloud.com>
2022-12-21 19:55:09 +0200

    T12-P03

---
+++ Technology Fundamentals with C# - 2022/T12_Arrays_Exercise/More_Exercise/P03_RecursiveFibonacci/P03_RecursiveFibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P03_RecursiveFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = int.Parse(Console.ReadLine());
+
+            int n = 0;
+            int n1 = 1;
+            int n2 = 1;
+
+            if (index == 1 || index == 2)
+            {
+                Console.WriteLine(n1);
+            }
+
+            for (int i = 2; i < index; i++)
+            {
+                n = n1 + n2;
+
+                n1 = n2;
+                n2 = n;
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
+
