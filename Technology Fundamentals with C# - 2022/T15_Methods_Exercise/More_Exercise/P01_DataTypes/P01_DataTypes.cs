662ad997fa774213c3140906c613dfce37bfe459
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-01-06 21:02:56 +0200

    T15-P01

---
+++ Technology Fundamentals with C# - 2022/T15_Methods_Exercise/More_Exercise/P01_DataTypes/P01_DataTypes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P01_DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            string data = Console.ReadLine();
+
+            if (dataType == "int")
+            {
+                int intNum = int.Parse(data);
+                DataProcessing(intNum);
+            }
+            else if (dataType == "real")
+            {
+                double realNum = double.Parse(data);
+                DataProcessing(realNum);
+            }
+            else
+            {
+                DataProcessing(data);
+            }
+        }
+
+        static void DataProcessing(int number)
+        {
+            int result = number * 2;
+            Console.WriteLine(result);
+        }
+
+        static void DataProcessing(double number)
+        {
+            double result = number * 1.5;
+            Console.WriteLine($"{result:F2}");
+        }
+
+        static void DataProcessing(string input)
+        {
+            Console.WriteLine("$" + input + "$");
+        }
+    }
+}
